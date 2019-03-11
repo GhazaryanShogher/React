8a3e111fb4d26530160657afa80799a3f6dabac6
GhazaryanShogher <shogherghazaryan4@gmail.com>
2019-03-11 23:46:12 +0400

    overlay

--- login/src/App.js
+++ login/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        <div id="overlay"></div>
+        <div className="form">
+          <h1>Registration</h1>
+          <div className="w3-rest">
+              <input  id="first" type="text" placeholder="First Name" required />
+          </div>
+        </div>
+
       </div>
     );
   }
